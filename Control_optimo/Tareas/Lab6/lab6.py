d0ad5fbc61d5c22f0c1287a3985b990ad3954eae
Wollyo <madarajoan@gmail.com>
2026-06-10 10:44:30 -0500

    Terminacion del reporte de lab

--- Control_optimo/Tareas/Lab6/lab6.py
+++ Control_optimo/Tareas/Lab6/lab6.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 from code6 import code6
 
-A = 1
-k = 2
-m = 0.1
-x0 = 10
+A = 5
+k = 10
+m = 0.2
+x0 = 0.8
 M = 1
 T = 20
+
 
 t, x, u = code6(A, k, m, x0, M, T)
 
@@ -16,16 +17,24 @@ plt.figure(figsize=(8,8))
 
 plt.subplot(3,1,1)
 plt.plot(t, x)
-plt.ylabel("Fish Concentration")
+plt.title("Evolución de la Concentración de Peces")
+plt.xlabel("Semanas")
+plt.ylabel("Concentración de Peces")
+plt.grid(True)
 
 plt.subplot(3,1,2)
 plt.plot(t, u)
-plt.ylabel("Harvesting Rate")
+plt.title("Tasa Óptima de Cosecha")
+plt.xlabel("Semanas")
+plt.ylabel("Tasa de Cosecha")
+plt.grid(True)
 
 plt.subplot(3,1,3)
 plt.plot(t, w)
-plt.ylabel("Average Weight")
-plt.xlabel("Weeks")
+plt.title("Peso Promedio de los Peces")
+plt.xlabel("Semanas")
+plt.ylabel("Peso Promedio")
+plt.grid(True)
 
 plt.tight_layout()
 plt.show()
